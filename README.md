4e59aa702ce7e19428b474906316faad051e0542
Mateus <mateusmreis92@gmail.com>
2021-08-13 07:27:15 -0300

    Clearing git cache.

---
+++ README.md
@@ -0,0 +1,115 @@
+# grancv
+
+Computer vision web application with granulometry purpouses, built on top of [ipyvuetify](https://github.com/mariobuikhuizen/ipyvuetify), [bqplot]([GitHub - bqplot/bqplot: Plotting library for IPython/Jupyter notebooks](https://github.com/bqplot/bqplot)), [opencv](https://github.com/opencv/opencv-python),  [ipycanvas](https://github.com/martinRenou/ipycanvas) and [lmfit](https://github.com/lmfit).
+
+![](/home/mop/Códigos/grancv/demo/gifs/grancv_gif.gif)
+
+## Install
+
+Create new conda environment:
+
+`conda create -n cv_env`
+
+Activate environment:
+
+`source activate cv_env`
+
+Install some packages:
+
+`python -m pip install -r requirements.txt`
+
+Run the app:
+
+`voila --VoilaConfiguration.extension_language_mapping='{".py": "python"}' $1 --debug`
+
+Or, if you prefer the dark theme:
+
+`voila --VoilaConfiguration.extension_language_mapping='{".py": "python"}' $1 --theme=dark --debug`
+
+You may want to paste this into your .bash_aliases file:
+
+```
+v() {
+  voila --VoilaConfiguration.extension_language_mapping='{".py": "python"}' $1 --theme=dark --debug
+}
+```
+
+Now, you can run the application with:
+
+`v index.py`
+
+## Operations Available
+
+You can use and tune combinations of the following operations:
+
+- Neural Style Transfer
+  
+  - Select one of the available torch .t7 models available in the 'models' folder.
+  
+  - You can adjust the quality of the final result by changing the image width.
+
+- Preprocessing
+  
+  - Filtering 
+    
+    - Simple
+    
+    - Bilateral Filtering
+  
+  - Blur
+    
+    - Simple
+    
+    - Median Blur 
+    
+    - Gaussian Blur
+
+- Morphologycal Operations
+  
+  - Erosion
+  
+  - Dilatation
+  
+  - Opening
+  
+  - Closing
+  
+  - Gradient
+  
+  - Top Hat
+  
+  - Black Hat
+
+- Thresholding
+  
+  - Simple
+    
+    - Binary
+    
+    - Binary Inverse
+    
+    - To-Zero
+    
+    - To-Zero Inverse
+  
+  - Adaptive
+    
+    - Adaptive mean c
+    
+    - Adaptive Gaussian c
+
+## To do
+
+This application is is very initial stage. More operations will be added to the application gradually. Here are some I've thought about:
+
+- Possibility of creating your own operation, from the combinations of wherever you want.
+
+- Choose (and plot) the kernel. All kernel used in the operations are matrices of kernels in the form:
+  
+  ```
+  [[1,1,1],
+  [1,1,1],
+  [1,1,1]]
+  ```
+
+- Scripts to build the application.
---
+++ app/thresholding.py
@@ -0,0 +1,114 @@
+import cv2
+import numpy as np
+import json
+
+import ipyvuetify as v
+from .vvapp.inputs import slider, select
+from .vvapp.outputs import container, row, column
+
+f = open('app/config.json')
+cfg = json.load(f)
+
+threshold_dropd = select(
+        items=cfg['options']['Thresholding'],
+        v_model='threshold-binary',
+        )
+threshold_slider = slider(
+        label='Threshold Value',
+        min=0,
+        max=255,
+        step=1,
+        v_model=127
+        )
+
+threshold_bs_slider = slider(
+        label='Block Size',
+        min=3,
+        max=21,
+        step=2,
+        v_model=9,
+        )
+threshold_C_slider = slider(
+        label='C',
+        min=-21,
+        max=21,
+        step=1,
+        v_model=7
+        )
+threshold_adapt_row = row(
+                children=[ threshold_bs_slider, threshold_C_slider ],
+                style_='display: none;'
+                )
+
+threshold_expp = v.ExpansionPanel(children=[
+    v.ExpansionPanelHeader(
+        children=['Thresholding']
+        ),
+    v.ExpansionPanelContent(children=[
+
+        threshold_dropd,
+
+        threshold_slider,
+
+        threshold_adapt_row,
+
+        ],
+        #style='\'
+        )
+    ], 
+    style_='display: none;'
+    )
+
+def threshold(img, op_type, value, bs, Cbs):
+
+    if op_type == 'threshold-binary':
+        ret,thresh = cv2.threshold(img,value,255,cv2.THRESH_BINARY)
+        return thresh
+    elif op_type == 'threshold-binary-inverse':
+        ret,thresh = cv2.threshold(img,value,255,cv2.THRESH_BINARY_INV)
+        return thresh
+    elif op_type == 'threshold-truncated':
+        ret,thresh = cv2.threshold(img,value,255,cv2.THRESH_TRUNC)
+        return thresh
+    elif op_type == 'threshold-tozero':
+        ret,thresh = cv2.threshold(img,value,255,cv2.THRESH_TOZERO)
+        return thresh
+    elif op_type == 'threshold-tozero-inverse':
+        ret,thresh = cv2.threshold(img,value,255,cv2.THRESH_TOZERO_INV)
+        return thresh
+    elif op_type=='threshold-adaptive-mean-c':
+        thresh = cv2.adaptiveThreshold( 
+                cv2.cvtColor(img, cv2.COLOR_RGB2GRAY),
+                255,
+                cv2.ADAPTIVE_THRESH_MEAN_C, 
+                cv2.THRESH_BINARY,
+                bs,
+                Cbs
+                )
+        return thresh
+    elif op_type=='threshold-adaptive-gaussian-c':
+        thresh = cv2.adaptiveThreshold(
+                cv2.cvtColor(img, cv2.COLOR_RGB2GRAY),
+                255,
+                cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                cv2.THRESH_BINARY,
+                bs,
+                Cbs
+                )
+        return thresh
+
+def update_adap_ts_items(*args):
+
+    if threshold_dropd.v_model not in \
+            ['threshold-adaptive-gaussian-c','threshold-adaptive-mean-c']:
+        threshold_adapt_row.style_='\
+                display: none; \
+                '
+        threshold_slider.style_='display: block'
+    else:
+        threshold_adapt_row.style_='\
+                display: block; \
+                '
+        threshold_slider.style_='display: none'
+threshold_dropd.on_event('change', update_adap_ts_items)
+
